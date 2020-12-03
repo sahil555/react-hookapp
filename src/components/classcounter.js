4fbd167547c5b0c6fab8f610b0cdb61ede2fef33
sahil555 <sahil555kureel@gmail.com>
2020-12-03 18:06:03 +0530

    asfdas

--- src/components/classcounter.js
+++ src/components/classcounter.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+// import http from ('http');
+import axios from 'axios';
 
 class Counter extends Component {
     constructor(props){
         super(props)
 
         this.state = {
-            count: 0
+            count: 0,
+            datafetch: []
         }
     }
 
@@ -17,12 +19,33 @@ class Counter extends Component {
         console.log(this.state.count)
     }
 
+    fetchdata =() =>{
+        let datafex = []
+            axios.get('http://localhost:4000/')
+              .then(data => {
+                this.datafex = data.data  
+                console.log(data.data); 
+            })
+
+            this.setState({
+                datafetch: datafex
+            }) 
+        }
+
     render(){
         return(
             <div>
                 State Count : {this.state.count}
                 <div>
                     <button onClick={this.increament}>Click</button>
+                </div>
+                <div>
+                    <button onClick={this.fetchdata}>Call API</button>
+                </div>
+                <div>
+               Fetched-Data : <ul>
+        {this.state.datafetch.map(data => <li> {data.data}</li>)}
+                   </ul>
                 </div>
             </div>
         )
